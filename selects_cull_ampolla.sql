f2a014451aa5aa66c422f29bac5d4630fb38cedf
Esther <githubEsther@gmail.com>
2021-02-22 18:09:39 +0100

    Esborrar fitxers de la M7

--- selects_cull_ampolla.sql
+++
@@ -1,8 +0,0 @@
-CREATE VIEW vistaClientes AS SELECT * FROM dadespersonals WHERE dadespersonals.tipus = 'client';
-
-select concat(DadesT.nom," ",DadesT.primerCognom) as 'Treballador', concat(DadesC.nom," ",DadesC.primerCognom) as 'Client', ulleres.tipusMontura from dadespersonals as DadesT 
-RIGHT outer join treballador_a_client on treballador_a_client.dadesPersonals_iddadesPersonals = DadesT.iddadesPersonals
-LEFT outer join dadespersonals as DadesC  on (DadesC.iddadesPersonals = treballador_a_client.dadesPersonals_iddadesPersonals1)
-LEFT outer join ulleres on treballador_a_client.idUlleres = ulleres.idulleres ;
-
-SELECT sum(ulleres.preu) FROM ulleres,treballador_a_client where ulleres.idulleres = treballador_a_client.idUlleres and ulleres.colorE = 'sense';
